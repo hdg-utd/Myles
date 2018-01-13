23cdb0e31abfef4fc3e0a54f1daa175286d9e547
hdg-utd <hdg150130@utdallas.edu>
2018-01-13 10:08:20 -0600

    major bug in way json formed solved

--- Backend/main.py
+++ Backend/main.py
@@ -8,11 +8,30 @@ from United_Airlines.united_airlines import UnitedAirlines
 
 from common.db_setup import AirlineDatabase
 
+
 airline_data = {}
-airline_data["aa"] = AmericanAirlines.get_aa_raw()
-airline_data["delta"] = DeltaAirlines.get_delta_raw()
-airline_data["united"] = UnitedAirlines.get_united_raw()
-airline_data["southwest"] = SouthwestAirlines.get_southwest_raw()
+aa_data = {}
+delta_data = {}
+united_data = {}
+southwest_data = {}
+
+for item in AmericanAirlines.get_aa_raw():
+    #print(list(item.keys()))
+    aa_data[list(item.keys())[0]] = item[list(item.keys())[0]]
+for item in DeltaAirlines.get_delta_raw():
+    #print(list(item.keys()))
+    delta_data[list(item.keys())[0]] = item[list(item.keys())[0]]
+for item in UnitedAirlines.get_united_raw():
+    #print(list(item.keys()))
+    united_data[list(item.keys())[0]] = item[list(item.keys())[0]]
+for item in SouthwestAirlines.get_southwest_raw():
+    #print(list(item.keys()))
+    southwest_data[list(item.keys())[0]] = item[list(item.keys())[0]]
+
+airline_data["aa"] = aa_data
+airline_data["delta"] = delta_data
+airline_data["united"] = united_data
+airline_data["southwest"] = southwest_data
 
 with open('./result_db.json', 'w') as fp:
     json.dump(airline_data, fp)
